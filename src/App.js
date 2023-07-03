cd7ffe31ddeacb5d66238a9d7af4cc2c7580f07c
karunakar6264 <ykarunakar999@gmail.com>
2023-07-03 14:26:54 +0530

    search movie posters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const App = () =>{
+  const[search,setSearch] = useState("");
+  const [data,setData] = useState([]);
+
+  const submitHandler = e => {
+    e.preventDefault();
+   fetch(`https://www.omdbapi.com/?s=${search}&apikey=263d22d8`).then(
+    response => response.json()
+   ).then(value => setData(value.Search))
+  }
+
+  const download = url => {
+    fetch(url).then(response => {
+      response.arrayBuffer().then(function(buffer){
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "image.png");
+        link.click();
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h2>Search your favorite movie</h2>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={search} onChange={(e) =>setSearch(e.target.value)}/> <br/>
+          <input type="submit" value="search"/>
+          <br />
+        </form>
+        <div className="row">
+        {data.map(movie=>
+        <div className="col-md-3">
+          <div className="card" style={{"width":"15rem"}}>
+          <img className="card-img-top" src={movie.Poster} alt={movie.Title}/>
+          <div className="card-body">
+            <h4 className="card-title">{movie.Title}</h4>
+            <a className="btn btn-primary" onClick={()=> download(movie.Poster)}>download Poster</a>
+            </div>
+          </div>
+        </div>
+          )}
+          </div>
+      </center>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
